d3c09210a4ada7b94b9458de12f8e0d03c1d6ecc
Fares Steen <fsteen@jabra.com>
2022-03-09 08:01:53 +0100

    init

---
+++ Before/Library/RefrenceBook.cs
@@ -0,0 +1,26 @@
+namespace Library;
+
+public class RefrenceBook:ILibraryItem
+{
+    public string Author { get; set; }
+    public DateTime BorrowDate { get; set; }
+    public string Borrower { get; set; }
+    public int CheckOutDurationInDays { get; set; } = 0;
+    public string LibraryId { get; set; }
+    public int Pages { get; set; }
+    public string Title { get; set; }
+    public void CheckIn()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void CheckOut(string borrower)
+    {
+        throw new NotImplementedException();
+    }
+
+    public DateTime GetDueDate()
+    {
+        return BorrowDate;
+    }
+}
